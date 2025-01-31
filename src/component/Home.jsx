d66d772f9e88bd1640da10ec7b16eb3ec6f001a8
najiubllah naziry <najibullah.nziry@gmail.com>
2025-01-31 12:17:39 +0500

    new file add

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ function Home(){
     return(
         <div>
             <h1>Home Page</h1>
-            <p>Welcome to the Home Page! fff</p>
+            <p>Welcome to thekkkk  Home Page! fff</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse quas reiciendis iusto modi qui itaque voluptatibus repellat illum quae, <NavLink to="https://www.youtube.com/watch?v=5vkoov53E0k&list=PL8p2I9GklV47BCAjiCtuV_liN9IwAl8pM&index=50">go to About page</NavLink></p>
         </div>
     )
