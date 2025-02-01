e7a176568d2d54d3a74fd6f250bda20210d356f4
najiubllah naziry <najibullah.nziry@gmail.com>
2025-02-01 12:57:23 +0500

    nut chf

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar(){
     return(
         <ul>
             <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/about">jj About</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
     )
