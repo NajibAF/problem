c38c8f43bea15f713fdbf42a871c3e249cf5abce
najiubllah naziry <najibullah.nziry@gmail.com>
2025-02-01 15:45:32 +0500

    css style commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,11 @@
 import {NavLink} from 'react-router-dom'
+import '../App.css'
 function Navbar(){
     return(
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+        <ul className='navbar'>
+            <li><NavLink style={{color:"red" , background:"yellow", fontSize:"33px"}} className="nav-bar-link" to="/">Home</NavLink></li>
+            <li><NavLink className="nav-bar-link" to="/about">About</NavLink></li>
+            <li><NavLink className="nav-bar-link" to="/contact">Contact</NavLink></li>
             
         </ul>
     )
