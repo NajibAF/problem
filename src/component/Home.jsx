72ad0aa5f83092f230b9b65d3410e5c350f05c4b
najiubllah naziry <najibullah.nziry@gmail.com>
2025-02-01 13:28:04 +0500

    nbnbn

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ function Home(){
         <div>
             <h1>Home Page</h1>
             <p>Welcome to the  Home Page! fff</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse quas reiciendis iusto modi qui itaque voluptatibus repellat illum quae, <NavLink to="https://www.youtube.com/watch?v=5vkoov53E0k&list=PL8p2I9GklV47BCAjiCtuV_liN9IwAl8pM&index=50">go to About page</NavLink></p>
+            <p>Lorem ipsum dolornn , sit amet consectetur adipisicing elit. Laboriosam esse quas reiciendis iusto modi qui itaque voluptatibus repellat illum quae, <NavLink to="https://www.youtube.com/watch?v=5vkoov53E0k&list=PL8p2I9GklV47BCAjiCtuV_liN9IwAl8pM&index=50">go to About page</NavLink></p>
         </div>
     )
 }
